89976e8d6c7d59ff512d77d231726f1ac2e6c277
gregory <10611760+gregoryguillou@users.noreply.github.com>
2022-11-10 21:56:12 +0100

    merge 1 and 2 threshold logic

--- contracts/examples/guards/InternetNativeCompanyGuard.sol
+++ contracts/examples/guards/InternetNativeCompanyGuard.sol
@@ -5,11 +5,68 @@ import "../../common/Enum.sol";
 import "../../base/GuardManager.sol";
 import "../../GnosisSafe.sol";
 import "hardhat/console.sol";
+import "openzeppelin4/access/Ownable.sol";
 
 /// @title Debug Transaction Guard - A guard that will emit events with extended information.
 /// @notice This guard is only meant as a development tool and example
 /// @author Richard Meissner - <richard@gnosis.pm>
-contract InternetNativeCompanyGuard is BaseGuard {
+contract InternetNativeCompanyGuard is Ownable, BaseGuard {
+
+    event SetPropertiesOnTarget(
+        address target,
+        bool allowed,
+        bool scoped,
+        bool delegateCallAllowed,
+        bool fallbackAllowed,
+        bool valueAllowed
+    );
+    event SetFunctionAllowedOnTarget(
+        address target,
+        bytes4 functionSig,
+        bool allowed
+    );
+
+    struct Target {
+        bool allowed;
+        bool scoped;
+        bool delegateCallAllowed;
+        bool fallbackAllowed;
+        bool valueAllowed;
+        mapping(bytes4 => bool) allowedFunctions;
+    }
+
+    mapping(address => Target) public allowedTargets;
+    mapping(address => bool) public unlockedSafe;
+
+    function setTargetProperties(address target, bool allow, bool scope, bool delegateCallAllow, bool fallbackAllow, bool valueAllow) public onlyOwner {
+        allowedTargets[target].allowed = allow;
+        allowedTargets[target].scoped = scope;
+        allowedTargets[target].delegateCallAllowed = delegateCallAllow;
+        allowedTargets[target].fallbackAllowed = fallbackAllow;
+        allowedTargets[target].valueAllowed = valueAllow;
+        emit SetPropertiesOnTarget(
+          target,
+          allowedTargets[target].allowed,
+          allowedTargets[target].scoped,
+          allowedTargets[target].delegateCallAllowed,
+          allowedTargets[target].fallbackAllowed,
+          allowedTargets[target].valueAllowed
+        );
+    }
+
+    function setAllowedFunction(
+        address target,
+        bytes4 functionSig,
+        bool allow
+    ) public onlyOwner {
+        allowedTargets[target].allowedFunctions[functionSig] = allow;
+        emit SetFunctionAllowedOnTarget(
+            target,
+            functionSig,
+            allowedTargets[target].allowedFunctions[functionSig]
+        );
+    }
+
     // solhint-disable-next-line payable-fallback
     fallback() external {
         // We don't revert on fallback to avoid issues in case of a Safe upgrade
@@ -21,23 +78,52 @@ contract InternetNativeCompanyGuard is BaseGuard {
         uint256 value,
         bytes memory data,
         Enum.Operation operation,
-        uint256 safeTxGas,
-        uint256 baseGas,
-        uint256 gasPrice,
-        address gasToken,
+        uint256,
+        uint256,
+        uint256,
+        address,
         // solhint-disable-next-line no-unused-vars
         address payable refundReceiver,
-        bytes memory signatures,
-        address msgSender
+        bytes memory,
+        address
     ) external override {
-          console.log("safe address is msg.sender:", msg.sender);
-          console.log("signatures are:");
-          console.logBytes(signatures); 
-          (bool success, bytes memory result) = msg.sender.call(abi.encodeWithSignature("getThreshold()"));
-          require(success, "You should be able to succesfully get the safe threshold");
-          (uint _threshold) = abi.decode(result, (uint));
-          console.log("current safe threshold:", _threshold);
-          require(_threshold > 1, "You must have at least 2 signers to execute a transaction");
+        console.log("safe address is msg.sender:", msg.sender);
+        if (unlockedSafe[msg.sender]) {
+          return();
+        }
+        (bool success, bytes memory result) = msg.sender.call(abi.encodeWithSignature("getThreshold()"));
+        require(success, "You should be able to succesfully get the safe threshold");
+        (uint _threshold) = abi.decode(result, (uint));
+        console.log("current safe threshold:", _threshold);
+        if (_threshold > 1) {
+          return();
+        }
+        require(
+            operation != Enum.Operation.DelegateCall ||
+                allowedTargets[to].delegateCallAllowed,
+            "Delegate call not allowed to this address"
+        );
+        require(allowedTargets[to].allowed, "Target address is not allowed");
+        if (value > 0) {
+            require(
+                allowedTargets[to].valueAllowed,
+                "Cannot send ETH to this target"
+            );
+        }
+        if (data.length >= 4) {
+            require(
+                !allowedTargets[to].scoped ||
+                    allowedTargets[to].allowedFunctions[bytes4(data)],
+                "Target function is not allowed"
+            );
+        } else {
+            require(data.length == 0, "Function signature too short");
+            require(
+                !allowedTargets[to].scoped ||
+                    allowedTargets[to].fallbackAllowed,
+                "Fallback not allowed for this address"
+            );
+        }
     }
 
     function checkAfterExecution(bytes32 txHash, bool success) external override {}
